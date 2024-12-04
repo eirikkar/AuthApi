52ae6b26d13a8dddabdd11e41d0f779d4f2f939c
eirikkar <eirikkar@proton.me>
2024-12-04 14:35:39 +0100

    Add migrations

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using AuthApi.Data;
 using AuthApi.Security;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<UserDbContext>(options =>
@@ -13,6 +12,12 @@ builder.Services.AddSingleton<HashPassword>();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+    db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
