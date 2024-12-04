dfd58b3aff841bc7ce80af3ef5c1f5f81f7e31c9
eirikkar <eirikkar@proton.me>
2024-12-04 10:07:31 +0100

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+public class User
+{
+  public Guid Id { get; set; }
+  public required string Username { get; set; }
+  public required string Password { get; set; }
+  public required string Email { get; set;}
+
+}
