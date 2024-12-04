178ad7a31c9277cd980025948d820bae53fc5346
eirikkar <eirikkar@proton.me>
2024-12-04 11:22:35 +0100

    Add HashPassword to main

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+using AuthApi.Security;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddSingleton<HashPassword>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
