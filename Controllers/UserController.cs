2273d04936c3662ac66e52d28c18a46422f51c7b
eirikkar <eirikkar@proton.me>
2024-12-04 11:34:06 +0100

    Moved new list inside constructor

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,29 +7,29 @@ using Microsoft.AspNetCore.Mvc;
 public class UserController : ControllerBase
 {
     private readonly HashPassword _hashPassword;
+    private List<User> _users;
 
     public UserController(HashPassword hashPassword)
     {
         _hashPassword = hashPassword;
+        _users = new List<User>
+        {
+            new User
+            {
+                Id = Guid.NewGuid(),
+                Username = "user1",
+                Password = _hashPassword.Hash("password1"),
+                Email = "something@something",
+            },
+            new User
+            {
+                Id = Guid.NewGuid(),
+                Username = "user2",
+                Password = "password2",
+                Email = "something@something",
+            },
+        };
     }
-
-    private List<User> _users = new List<User>
-    {
-        new User
-        {
-            Id = new Guid(),
-            Username = "user1",
-            Password = "password1",
-            Email = "something@something",
-        },
-        new User
-        {
-            Id = new Guid(),
-            Username = "user2",
-            Password = "password2",
-            Email = "something@something",
-        },
-    };
 
     [HttpGet]
     public IActionResult Get()
