509905de04fb445fa4d17a68d133302ecc300781
eirikkar <eirikkar@proton.me>
2024-12-04 14:27:49 +0100

    Refactor UserController to include database

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using AuthApi.Data;
 using AuthApi.Models;
 using AuthApi.Security;
 using Microsoft.AspNetCore.Mvc;
@@ -8,9 +9,11 @@ public class UserController : ControllerBase
 {
     private readonly HashPassword _hashPassword;
     private List<User> _users;
+    public readonly UserDbContext _context;
 
-    public UserController(HashPassword hashPassword)
+    public UserController(HashPassword hashPassword, UserDbContext userDbContext)
     {
+        _context = userDbContext;
         _hashPassword = hashPassword;
         _users = new List<User>
         {
@@ -31,9 +34,24 @@ public class UserController : ControllerBase
         };
     }
 
-    [HttpGet]
-    public IActionResult Get()
+    [HttpPost]
+    public IActionResult Post([FromBody] User _user)
     {
-        return Ok(_users);
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Username = _user.Username,
+            Password = _hashPassword.Hash(_user.Password),
+            Email = _user.Email,
+        };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(Post), user);
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<User>> Get()
+    {
+        return _context.Users.ToList();
     }
 }
