4116e8155c17d73776255e1bb18ca56732edfb6e
eirikkar <eirikkar@proton.me>
2024-12-04 10:10:08 +0100

    Add namespace

--- Models/Topic.cs
+++ Models/Topic.cs
@@ -1,9 +1,10 @@
+namespace AuthApi.Models;
 public class Topic
 {
-  public Guid Id {get; set;}
-  public required string Title {get; set;}
-  public required string Content {get; set;}
-  public required string ImageUrl {get; set;}
-  public required DateTime CreatedAt {get; set;}
-  public required User CreatedBy {get; set;}
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public required string Content { get; set; }
+    public required string ImageUrl { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required User CreatedBy { get; set; }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,8 @@
+namespace AuthApi.Models;
 public class User
 {
-  public Guid Id { get; set; }
-  public required string Username { get; set; }
-  public required string Password { get; set; }
-  public required string Email { get; set;}
-
+    public Guid Id { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public required string Email { get; set; }
 }
